4f7e7e192e0b5528410d7b93687c68b35df480a6
Vlad Bolkunov <gloryson@mail.ru>
2023-03-13 07:21:14 +0100

    feat: added new opportunities

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-const express = require('express');
-const fs = require('fs/promises');
-const { constants } = require('fs');
+import express from "express";
+import { __dirname } from './__dirname.js';
+import mongodb from 'mongodb';
 
 let app = express();
-
-
-app.get('/page/:num/', async (req, res) => {
-  let result = `${__dirname}/index${req.params.num}.html`;
-
- try {
-  await fs.access(result, constants.F_OK);
-  res.sendFile(result);
- } catch {
-  res.status(404).send('Error')
- }
-
-});
+let mongoClient = new mongodb.MongoClient('mongodb://127.0.0.1:27017/');
 
 app.use(express.static(__dirname + '/'));
 
 app.listen(3000, () => {
   console.log('running');
 });
+
+const connect = async () => {
+	try {
+		let db = mongoClient.db('test');
+		let coll = db.collection('users');
+		
+		app.get('/users', async function(req, res) {
+      let users = await coll.find().toArray();
+      res.send(users);
+    });
+		
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+connect();
